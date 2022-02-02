ff83597c1c0fe4e7287948950af97ed16d8555a0
clinardjw <jon.clinard@yahoo.com>
2022-02-02 01:16:32 -0500

    second iteration complete

--- src/main/java/virtual_pet/Snake.java
+++ src/main/java/virtual_pet/Snake.java
@@ -5,14 +5,14 @@ public class Snake {
     private String color;
     private int age;
     private int length;
-    private boolean hungry;
+    private int hunger;
 
-    public Snake(String name, String color, int age, int length, boolean hungry) {
+    public Snake(String name, String color, int age, int length, int hunger) {
         this.name = name;
         this.color = color;
         this.age = age;
         this.length = length;
-        this.hungry = hungry;
+        this.hunger = hunger;
     }
 
     public String getName() {
@@ -31,42 +31,50 @@ public class Snake {
         return length;
     }
 
-    public boolean isHungry() {
-        return hungry;
-    }
-    public void feed(){
-        hungry = false;
+    public int isHungry() {
+        return hunger;
     }
 
     public String status(){
         String hungryMessage = "";
-        if(hungry){
+        if(hunger >= 75){
             hungryMessage = name + " is hungry!";
         }
         else{
-            hungryMessage = name + " is not hungry";
+            hungryMessage = name + " is not hungry.";
         }
         return name + " is a " + color + " Snake. It is " + age + " years old and " + length + " inches long. "+ hungryMessage;
     }
 
     public void playing(){
-        if ( hungry == true ){
-            System.out.println("Your pet is already hungry, you should feed it!");
+        if ( hunger >= 75 ){
+            System.out.println("Your pet is hungry, you should feed it!");
         }
         else {
             System.out.println("You play with your pet. It seems to be getting hungry!");
-                hungry = true;
+                hunger += 25;
         }
     }
     public void feeding(){
-        if ( hungry = true ){
-            System.out.println("Your feed your pet. He seems to have grown!");
-            hungry = false;
+        if (hunger >= 75){
+            System.out.println("Your feed " + name + ". It seems to have grown!");
+            hunger -= 25;
             length += 1;
         }
         else {
             System.out.println("Your snake is not hungry right now.");
         }
+    }
+    public void tick() {
+        length++;
+    }
+    public void showSnake(){
+        System.out.print(">-");
+        for(int i =0;i<length;i++){
+
+            System.out.print("░");
+        }
+        System.out.println("");
     }
 }
 
--- src/main/java/virtual_pet/VirtualPetApplication.java
+++ src/main/java/virtual_pet/VirtualPetApplication.java
@@ -1,31 +1,69 @@
 package virtual_pet;
 
-import java.time.Clock;
 import java.util.Scanner;
-import java.util.Timer;
-import java.util.TimerTask;
 
 public class VirtualPetApplication {
-    Snake jake = new Snake("Jake", "green", 3, 6, false);
+    Snake jake = new Snake("Jake", "green", 3, 6, 75);
+    Snake phil = new Snake("Phil","blue",4,8,75);
+    Snake jaydee = new Snake("Jaydee", "red",7, 16, 75);
+    Snake devin = new Snake("Devin", "orange",7, 18,75);
+
+    //arraylist to select
+    Snake[] Snakes = {jake, phil, jaydee, devin};
+    Snake activeSnake = Snakes[00];
 
     public static void main(String[] args) {
         VirtualPetApplication myVirtualPetApplication = new VirtualPetApplication();
         myVirtualPetApplication.snakeTestMethod();
     }
-    public void snakeTestMethod() {
-        System.out.println(jake.status());
+    //user selects snake
+    public void snakeSelectMethod() {
         Scanner userInput = new Scanner(System.in);
-        Clock baseClock = Clock.systemUTC();
+        System.out.println("You do not currently have a snake. To adopt Jake, press '1'. To adopt Phil, press '2'. To adopt Jaydee, press '3'. To adopt Devin, press '4'.");
+        int selectedSnake = userInput.nextInt();
+        if (selectedSnake == 1) {
+            activeSnake = jake;
+            System.out.println("You have adopted Jake.");
+        }else if (selectedSnake == 2) {
+            activeSnake = phil;
+            System.out.println("You have adopted Phil.");
+        }else if (selectedSnake == 3) {
+            activeSnake = jaydee;
+            System.out.println("You have adopted Jaydee.");
+        }else if (selectedSnake == 4) {
+            activeSnake = devin;
+            System.out.println("You have adopted Devin.");
+        }
+    }
+
+    //user interacts with snake
+    public void snakeTestMethod() {
+        System.out.println(activeSnake.status());
+        Scanner userInput = new Scanner(System.in);
         while (true) {
-            System.out.println("To play with your pet, type '1'. To feed your pet, type '2'. To check the status of your pet, type '3'. To quit, press '4'.");
+            System.out.println("To play with your pet, type '1'. To feed your pet, type '2'. To check the status of your pet, type '3'. To check the status of all pets, press '4'. To feed all pets, press '5'. To return your current pet to the shelter, press '6'. To quit, press '7'.");
+            activeSnake.showSnake();
             int action = userInput.nextInt();
             if (action == 1) {
-                jake.playing();
+                activeSnake.playing();
             } else if (action == 2) {
-                jake.feeding();
+                activeSnake.feeding();
+                activeSnake.tick();
             } else if (action == 3) {
-                System.out.println(jake.status());
+                System.out.println(activeSnake.status());
             } else if (action == 4) {
+                System.out.println(jake.status());
+                System.out.println(phil.status());
+                System.out.println(jaydee.status());
+                System.out.println(devin.status());
+            }else if (action == 5) {
+                    jake.feeding();
+                    phil.feeding();
+                    jaydee.feeding();
+                    devin.feeding();
+            } else if (action == 6) {
+                snakeSelectMethod();
+            } else if (action == 7) {
                 break;
             } else {
                 System.out.println("Please choose a valid input!");
